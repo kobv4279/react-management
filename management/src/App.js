e403799ff8584314f0aa70fada2c6e61e4166b38
kobv4279 <kobv4278@gmail.com>
2021-01-31 20:10:57 +0900

    first commit

---
+++ management/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="gray=background">
+      
+      <img src={logo} alt="logo" />
+        <p>
+          Let's develop management system
+        </p>
+        
+    
+    </div>
+  );
+}
+
+export default App;
